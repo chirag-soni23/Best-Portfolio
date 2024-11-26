0326d9128dc4a499bf0745e4c5414a05efabe0f9
Chirag Soni <csoni0693@gmail.com>
2024-11-26 08:12:38 +0530

    portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 const Home = () => {
+    const {darkMode} = useContext(ThemeContext);
     const texts = ["MERN Stack Developer","Frontend Developer","Javascript Expert"];
     const [currentText,setCurrentText] = useState(texts[0]);
 
@@ -20,28 +22,28 @@ const Home = () => {
             <div className="relative text-center">
                 {/* Foreground Text */}
                 <h4 className="relative tracking-[5px] z-10 right-3 font-[first] text-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-                    <h4 className="uppercase text-pretty text-6xl sm:text-7xl md:text-8xl lg:text-8xl mt-2 font-[first]">hey, i'm</h4>
-                    <h4 className="uppercase text-pretty text-6xl sm:text-7xl md:text-8xl lg:text-8xl mt-2 font-[first]">chirag</h4>
+                    <h4 className={`uppercase text-pretty text-6xl sm:text-7xl md:text-8xl ${darkMode ? "text-[#F1F3DF]" : "text-black"} lg:text-8xl mt-2 font-[first]`}>hey, i'm</h4>
+                    <h4 className={`uppercase text-pretty text-6xl sm:text-7xl md:text-8xl ${darkMode ? "text-[#F1F3DF]" : "text-black"} lg:text-8xl mt-2 font-[first]`}>chirag</h4>
                 </h4>
                 {/* Realistic Shadow Effect */}
-                <h4 className="absolute top-3 tracking-[5px] font-[first] text-[#DDA15F] z-0 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-                    <h4 className="uppercase text-pretty text-6xl sm:text-7xl md:text-8xl lg:text-8xl mt-2 font-[first]">hey, i'm</h4>
-                    <h4 className="uppercase text-pretty text-6xl sm:text-7xl md:text-8xl lg:text-8xl mt-2 font-[first]">chirag</h4>
+                <h4 className={`absolute top-3 tracking-[5px] font-[first] text-[#DDA15F] z-0 text-4xl sm:text-5xl md:text-6xl lg:text-7xl`}>
+                    <h4 className={`uppercase text-pretty text-6xl sm:text-7xl md:text-8xl lg:text-8xl mt-2 font-[first]`}>hey, i'm</h4>
+                    <h4 className={`uppercase text-pretty text-6xl sm:text-7xl md:text-8xl lg:text-8xl mt-2 font-[first]`}>chirag</h4>
                 </h4>
             </div>
 
             {/* Scroll Indicator */}
-            <div className="absolute bottom-14 right-14 rotate-90 tracking-[10px] font-[first] flex flex-col uppercase text-gray-600 text-sm sm:text-base md:text-lg">
+            <div className={`absolute bottom-14 right-14 rotate-90 tracking-[10px] font-[first] flex flex-col uppercase ${darkMode ? "text-[#F1F3DF]":"text-gray-600"} text-sm sm:text-base md:text-lg`}>
                 <h4>scroll</h4>
-                <span className="w-full bg-black h-[1px] animate-lineMove"></span>
+                <span className={`w-full ${darkMode ? "bg-[#F1F3DF]":"bg-black"} h-[1px] animate-lineMove`}></span>
             </div>
 
             {/* Subtitle */}
             <div className="mt-4 w-[60%] sm:w-[50%] md:w-[40%] lg:w-[30%]">
-                <p className="text-gray-700 text-sm tracking-[5px] uppercase lg:text-base text-center">
+                <p className={`text-sm tracking-[5px] uppercase lg:text-base text-center ${darkMode ? "text-[#F1F3DF]":"text-gray-700"}`}>
                     {currentText}
                 </p>
-                <div className="w-full bg-black animate-textLine relative bottom-6 h-full"></div>
+                <div className={`w-full ${darkMode ? "bg-[#F1F3DF]":"bg-black"} animate-textLine relative bottom-6 h-full`}></div>
             </div>
         </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import gsap from "gsap";
+import { ThemeContext } from "../context/ThemeContext";
 
 const Navbar = () => {
+  const { darkMode } = useContext(ThemeContext);
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef(null);
   const linksRef = useRef([]);
@@ -70,7 +72,7 @@ const Navbar = () => {
       <div
         ref={overlayRef}
         className="fixed top-0 left-0 w-full h-full bg-black z-40"
-        style={{ transform: "translateY(100%)" }} // Initial position (hidden at bottom)
+        style={{ transform: "translateY(100%)" }} 
       ></div>
 
       <div className="fixed flex justify-between items-center z-50 bg-transparent top-0 left-0 right-0 font-[first] lg:justify-between backdrop-blur-sm py-5 px-20">
@@ -80,17 +82,17 @@ const Navbar = () => {
         {/* Hamburger Menu */}
         <div className="relative h-6 w-10 cursor-pointer" onClick={toggleMenu}>
           <div
-            className={`absolute top-0 left-0 h-[2px] w-10 bg-black transition-transform ${
+            className={`absolute top-0 left-0 h-[2px] w-10 ${darkMode ? "bg-[#F1F3DF]":"bg-black"} transition-transform ${
               isOpen ? "rotate-45 bg-white translate-y-2.5 w-6 ml-2" : ""
             }`}
           ></div>
           <div
-            className={`absolute top-2.5 left-2 h-[2px] w-6 bg-black transition-transform ${
-              isOpen ? "-rotate-45 bg-white -translate-y-3.6 w-6" : ""
+            className={`absolute top-2.5 left-2 h-[2px] w-6 ${darkMode ? "bg-[#F1F3DF]":"bg-black"} transition-transform ${
+              isOpen ? `-rotate-45 bg-white -translate-y-3.6 w-6` : ``
             }`}
           ></div>
           <div
-            className={`absolute top-1.2 left-0 h-[2px] w-10 bg-black transition-all ${
+            className={`absolute top-1.2 left-0 h-[2px] w-10 ${darkMode ? "bg-[#F1F3DF]":"bg-black"}  transition-all ${
               isOpen ? "opacity-0" : "opacity-100"
             }`}
           ></div>
@@ -99,7 +101,7 @@ const Navbar = () => {
         {/* Dropdown Menu */}
         <div
           ref={menuRef}
-          className={`absolute top-2 -z-10 right-20 bg-black shadow-lg rounded-lg p-10 space-y-3 text-left ${
+          className={`absolute top-2 -z-10 right-20 ${darkMode ? "bg-[#F1F3DF]":"bg-black"} shadow-lg rounded-lg p-10 space-y-3 text-left ${
             isOpen ? "block" : "hidden"
           }`}
         >
@@ -109,22 +111,22 @@ const Navbar = () => {
           >
             <button
               onClick={() => handleLinkClick("/")}
-              className="block text-white text-sm font-medium transition group"
+              className={`${darkMode ? "text-black":"text-[#F1F3DF]"} block text-sm font-medium transition group`}
             >
               Home
-              <span className="block bg-white rounded-sm h-[2px] w-1 mx-auto mt-1 transition-all duration-300 group-hover:w-full"></span>
+              <span className={`block ${darkMode ? "bg-black":"bg-[#F1F3DF]"} rounded-sm h-[2px] w-1 mx-auto mt-1 transition-all duration-300 group-hover:w-full`}></span>
             </button>
           </div>
           <div
             ref={(el) => (linksRef.current[1] = el)}
-            className="text-white text-sm font-medium transition group"
+            className={`${darkMode ? "text-black":"text-[#F1F3DF]"} text-sm font-medium transition group`}
           >
             <button
               onClick={() => handleLinkClick("/project")}
-              className="block text-white text-sm font-medium transition group"
+              className={`block ${darkMode ? "text-black":"text-[#F1F3DF]"}  text-sm font-medium transition group`}
             >
               Projects
-              <span className="block bg-white rounded-sm h-[2px] w-1 mx-auto mt-1 transition-all duration-300 group-hover:w-full"></span>
+              <span className={`block ${darkMode ? "bg-black":"bg-[#F1F3DF]"} rounded-sm h-[2px] w-1 mx-auto mt-1 transition-all duration-300 group-hover:w-full`}></span>
             </button>
           </div>
           <div
@@ -133,10 +135,10 @@ const Navbar = () => {
           >
             <button
               onClick={() => handleLinkClick("/contact")}
-              className="block text-white text-sm font-medium transition group"
+              className={`block text-sm font-medium transition group ${darkMode ? "text-black":"text-[#F1F3DF]"}`}
             >
               Contact
-              <span className="block bg-white rounded-sm h-[2px] w-1 mx-auto mt-1 transition-all duration-300 group-hover:w-full"></span>
+              <span className={`block ${darkMode ? "bg-black":"bg-[#F1F3DF]"} rounded-sm h-[2px] w-1 mx-auto mt-1 transition-all duration-300 group-hover:w-full`}></span>
             </button>
           </div>
         </div>
