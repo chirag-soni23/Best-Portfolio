cd37560ecced4a45c92fb42ca149a15985916495
Chirag Soni <csoni0693@gmail.com>
2024-11-26 11:35:15 +0530

    portfolio

--- src/Theme/DarkLightTheme.jsx
+++ src/Theme/DarkLightTheme.jsx
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react';
-import { FaSun, FaMoon } from 'react-icons/fa';
+import { FaMoon } from 'react-icons/fa';
 import { ThemeContext } from '../context/ThemeContext';
+import { IoSunnySharp } from "react-icons/io5";
 
 const DarkLightTheme = () => {
   const { darkMode, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <div onClick={toggleTheme} className="theme-toggle fixed bg-black flex justify-center items-center left-4 bottom-4 rounded-full p-2 cursor-pointer">
+    <div 
+      onClick={toggleTheme} 
+      className={`theme-toggle fixed ${darkMode ? "bg-[#F1F3DF]":"bg-black"} flex justify-center items-center left-4 bottom-4 rounded-full p-2 cursor-pointer`}
+    >
       <button className="theme-button cursor-pointer">
-        {darkMode ? <FaSun size={24} color="#FFD700" /> : <FaMoon size={24} color="#1E90FF" />}
+        {darkMode ? (
+          <IoSunnySharp size={24} fill="none" stroke="#000" strokeWidth={20} />
+        ) : (
+          <FaMoon size={24} fill="none" stroke="#fff" strokeWidth={20} />
+        )}
       </button>
     </div>
   );
