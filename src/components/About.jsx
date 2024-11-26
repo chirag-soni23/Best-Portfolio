57be5bf415a31ba9141c94bccddf8be04c7e6116
Chirag Soni <csoni0693@gmail.com>
2024-11-26 08:21:03 +0530

    portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { gsap } from 'gsap';
+import { ThemeContext } from '../context/ThemeContext';
 
 const About = () => {
+  const { darkMode } = useContext(ThemeContext);
   const overlayRef = useRef(null);
   const location = useLocation();
 
@@ -36,7 +38,7 @@ const About = () => {
 
       <div className="relative text-center">
         {/* Foreground Text */}
-        <h4 className="relative tracking-[5px] z-10 right-3 font-[first] text-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl uppercase">
+        <h4 className={`relative tracking-[5px] z-10 right-3 font-[first] ${darkMode ? "text-[#F1F3DF]" : "text-black"}text-4xl sm:text-5xl md:text-6xl lg:text-7xl uppercase`}>
           who am i
         </h4>
         {/* Realistic Shadow Effect */}
@@ -46,16 +48,16 @@ const About = () => {
       </div>
 
       {/* Description Paragraph */}
-      <p className="font-[second] text-sm sm:text-base md:text-lg lg:text-xl text-center text-gray-700 mt-10 max-w-[80%] sm:max-w-[70%] md:max-w-[60%] lg:max-w-[100%] tracking-widest">
+      <p className={`font-[second] text-sm sm:text-base md:text-lg lg:text-xl text-center mt-10 ${darkMode ? "text-[#F1F3DF]":"text-gray-700"} max-w-[80%] sm:max-w-[70%] md:max-w-[60%] lg:max-w-[100%] tracking-widest`}>
         Hello, I'm Chirag, a web designer, React front-end developer, and content creator. I specialize in creating stunning landing pages, websites, and dynamic front-end applications for businesses worldwide. If you're looking for a modern and impactful website, feel free to email me. If we are a good fit, I'll provide a time and cost estimate for your project.
       </p>
 
       <div className="relative mt-10 flex items-center">
-        <div className="bg-black text-[#DDA15F] p-2">
+        <div className={`text-[#DDA15F] ${darkMode ? "bg-[#F1F3DF]":"bg-black"} p-2`}>
           <h2 className="font-[second]">see my work</h2>
         </div>
-        <div className="border-2 absolute w-full h-full top-1 left-2 border-black p-2"></div>
-        <div className="w-8 absolute left-36 h-8 flex items-center justify-center rounded-full border-2 border-black">
+        <div className={`border-2 absolute w-full h-full top-2 left-2 ${darkMode ? "border-[#F1F3DF]":"border-black"} -z-10 p-2`}></div>
+        <div className={`w-8 absolute left-36 h-8 flex items-center justify-center rounded-full border-2 ${darkMode ? "border-[#F1F3DF]":"border-black"}`}>
           <Link to="/project" className="text-[#DDA15F] flex items-center justify-center">
             +
           </Link>
