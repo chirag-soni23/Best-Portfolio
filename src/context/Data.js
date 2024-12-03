92893362f1c46e1685336c76b1f3ae413dfc028c
Chirag Soni <csoni0693@gmail.com>
2024-12-03 16:23:27 +0530

    portfolio

--- src/context/Data.js
+++ src/context/Data.js
@@ -1,17 +1,17 @@
 import food from '../assets/food.png';
 import LandingPage from '../assets/LandingPage.png';
 import Calculator from '../assets/Calculator.png'
+import Portfolio from '../assets/Portfolio.png'
 import myBanki from '../assets/myBanki.jpg';
-import trillo from '../assets/trillo.jpg';
 export const Data = [
     {
       id: 1,
-      image: food,
-      name: "FoodSearch",
-      description: "Food search app is a React-based platform that uses an API to help users find their favorite dishes and restaurants. Adding features like a loader, search suggestions, and filters can enhance the user experience further",
-      feature:["HTML","Tailwind Css","React"],
-      code:"https://github.com/chirag-soni23/food-search-app",
-      hosting:"https://food-search-app-rho.vercel.app/"
+      image: Portfolio,
+      name: "MyPortfolio",
+      description: "Creative web portfolio designed with HTML, Tailwind CSS, and Three.js. It features modern, responsive layouts and sleek user interfaces. Using Three.js, interactive 3D visualizations bring the design to life. The portfolio demonstrates my skills in front-end development and cutting-edge web technologies.",
+      feature:["HTML","Tailwind Css","React","Three.js"],
+      code:"https://github.com/chirag-soni23/Amazing-Portfolio",
+      hosting:"https://amazing-portfolio-one.vercel.app/"
     },
     {
       id: 2,
@@ -33,6 +33,15 @@ export const Data = [
     },
     {
       id: 4,
+      image: food,
+      name: "FoodSearch",
+      description: "Food search app is a React-based platform that uses an API to help users find their favorite dishes and restaurants. Adding features like a loader, search suggestions, and filters can enhance the user experience further",
+      feature:["HTML","Tailwind Css","React"],
+      code:"https://github.com/chirag-soni23/food-search-app",
+      hosting:"https://food-search-app-rho.vercel.app/"
+    },
+    {
+      id: 5,
       image: myBanki,
       name: "myBanki",
       description: "myBanki is a fun project that simulates an online banking platform. It consists of a static webpage providing bank information such as features and pricing, along with a user-friendly dashboard where users can view their balance and perform various actions.",
@@ -40,16 +49,7 @@ export const Data = [
        code:"",
       hosting:""
     },
-    {
-      id: 1,
-      image: trillo,
-      name: "trillo",
-      description: "trillo is a simple one-page hotel booking landing page.",
-      feature:["HTML","CSS","SASS"],
-       code:"https://github.com/chirag-soni23/food-search-app",
-      hosting:"https://food-search-app-rho.vercel.app/"
-    },
-  
+
    
   ];
 
