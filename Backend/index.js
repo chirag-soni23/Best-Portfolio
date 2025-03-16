8069ba376dc8648bfd1be5b3de349fc1a8a4c0f2
Chirag Soni <csoni0693@gmail.com>
2025-03-16 16:23:02 +0530

    library

--- Backend/index.js
+++ Backend/index.js
@@ -12,41 +12,41 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(compression());
 
-app.post("/send-email",async(req,res)=>{
-    const {name,email,subject,message} = req.body;
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
-    
-      const mailOptions = {
-        from: email,
-        to: process.env.EMAIL_USER,
-        subject: `New Contact Form Message: ${subject}`,
-        text: `From: ${name} \nEmail: ${email} \nMessage: ${message}`,
-      };
-    
-      try {
-        await transporter.sendMail(mailOptions);
-        res.status(200).json({ message: 'Email sent successfully!' });
-      } catch (error) {
-        res.status(500).json({ message: 'Failed to send email', error });
-      }
+app.post("/send-email", async (req, res) => {
+  const { name, email, subject, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: `New Contact Form Message: ${subject}`,
+    text: `From: ${name} \nEmail: ${email} \nMessage: ${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Email sent successfully!' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to send email', error });
+  }
 })
 
 const dirname = path.resolve();
-app.use(express.static(path.join(dirname,"/Frontend/dist")))
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(dirname,"Frontend","dist","index.html"))
+app.use(express.static(path.join(dirname, "/Frontend/dist")))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(dirname, "Frontend", "dist", "index.html"))
 })
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send("Hello")
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port: ${PORT} http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT} http://localhost:${PORT}`);
 })
