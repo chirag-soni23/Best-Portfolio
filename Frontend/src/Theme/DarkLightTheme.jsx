3499b7f4e3f0da137588ee9c633f529fc9074644
Chirag Soni <csoni0693@gmail.com>
2025-09-05 14:15:33 +0530

    modified

--- Frontend/src/Theme/DarkLightTheme.jsx
+++ Frontend/src/Theme/DarkLightTheme.jsx
@@ -1,21 +1,22 @@
-import React, { useContext } from 'react';
-import { FaMoon } from 'react-icons/fa';
-import { ThemeContext } from '../context/ThemeContext';
-import { IoSunnySharp } from "react-icons/io5";
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import { Sun, Moon } from "lucide-react";
 
 const DarkLightTheme = () => {
   const { darkMode, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <div 
-      onClick={toggleTheme} 
-      className={`theme-toggle fixed ${darkMode ? "bg-[#F1F3DF]":"bg-black"} flex justify-center items-center left-4 bottom-4 rounded-full p-2 cursor-pointer`}
+    <div
+      onClick={toggleTheme}
+      className={`theme-toggle fixed ${
+        darkMode ? "bg-[#F1F3DF]" : "bg-black"
+      } flex justify-center items-center left-4 bottom-4 rounded-full p-2 cursor-pointer`}
     >
       <button className="theme-button cursor-pointer">
         {darkMode ? (
-          <IoSunnySharp size={24} fill="none" stroke="#000" strokeWidth={20} />
+          <Sun className="w-6 h-6 text-black" />
         ) : (
-          <FaMoon size={24} fill="none" stroke="#fff" strokeWidth={20} />
+          <Moon className="w-6 h-6 text-white" />
         )}
       </button>
     </div>
